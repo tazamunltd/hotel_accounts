5a5a87d393ccaa7db5f3024f2d3d29f21a7661b1
tazamunltd <dev@tazamun.com.sa>
2025-05-17 15:48:01 +0300

    remove the tz_front_desk module from addons folder

--- addons/tz_front_desk/models/group_booking.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class GroupBooking(models.Model):
-    _inherit = 'group.booking'
-
-    description = fields.Text(string='Description')
-    discount = fields.Float(string='Discount (%)')
-    reopen = fields.Boolean(string='Open or reopen a group account automatically')
-    obsolete = fields.Boolean(string='Obsolete')
